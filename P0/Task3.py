804a2e2f93ea423dcdf795b1a39886e008ab8540
Marco Tello <muchahuev@hotmail.com>
2019-05-13 17:34:33 -0500

    finishing the task 3 and 4

--- P0/Task3.py
+++ P0/Task3.py
@@ -44,9 +44,9 @@ to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
 
-def get_numbers_called_by_area_code(area_code, calls):
+def get_unique_numbers_called_by_area_code(area_code, calls):
     """
-    This function returns a list of area codes calles by an specific area code.
+    This function returns a list of area codes called by an specific area code.
 
     INPUT:
     area_code: the area code where we want to filter the list
@@ -56,65 +56,165 @@ def get_numbers_called_by_area_code(area_code, calls):
     """
 
     # I have chosen set as my data collection because for this problem the order doesn't matter. Set holds unique hashable object like strings.
-    # Creating the set - O(1)
+    # Creating the set  - O(1)
     called_numbers = set()
     
-    # the index for iterating the list of calls. It starts on 0 - O(1)
+    # the index for iterating the list of calls. It starts on 0  - O(1)
     index = 0
 
-    # iterating over the list of calls - O(n)
+    # iterating over the list of calls  - O(n)
     while index < len(calls):
-        # compare if the caller number starts with the area code - O(1)
+        # compare if the caller number starts with the area code  - O(1)
         if calls[index][0].startswith(area_code):
-            # if the caller number starts with the area code, then the reciever number is add to the set - O(4)
+            # if the caller number starts with the area code, then the reciever number is add to the set  - O(4)
             # Also, the filter_area_codes function is calledx to filter just the area code of the receiver number. 
             called_numbers.add(filter_area_codes(calls[index][1]))
         
-        # increasing the index by 1 - O(1)
+        # increasing the index by 1  - O(1)
         index += 1
 
-    # returning a list of area codes based on the set - O(1)
+    # returning a list of area codes based on the set  - O(1)
     return list(called_numbers)
 
+def get_number_of_calls_from_area_code(area_code, calls):
+    """
+    This function returns the number of calls from a specific area code.
+
+    INPUT:
+    area_code: the area code where we want to filter the list
+    calls: the list of calls
+
+    RETURN: the number of calls
+    """
+    # the index for iterating the list of calls. It starts on 0  - O(1)
+    index = 0
+    # the number of calls, in this case starts with 0  - O(1)
+    number_of_calls = 0
+
+    # iterating over the list of calls  - O(n)
+    while index < len(calls):
+        # compare if the caller number starts with the area code  - O(1)
+        if calls[index][0].startswith(area_code):
+            # if the caller number starts with the area code, then the number of calls is icreased  - O(4)
+            number_of_calls += 1
+        
+        # increasing the index by 1  - O(1)
+        index += 1
+
+    # returning a list of area codes based on the set  - O(1)
+    return number_of_calls
+
 def filter_area_codes(number):
+    """
+    This function returns the area core of a given phone number
+
+    INPUT:
+    number: a phone number. 
+
+    RETURN: the area code if it's a velid phone number.
+    """
+    # True in case the phone number starts with (  - O(1)
     if number.startswith('('):
+        # returns the area code  - O(1)
         return number[:number.find(')')+1]
+    # True in case the phone number has a space in the middle  - O(1)
     elif number.find(' ') > 0:
-        if number.startswith('7') or number.startswith('8') or number.startswith('9'):
+         # True in case the phone number starts with 7 or 8 or 9  - O(1)
+        if number.startswith('7') or number.startswith('8') or number.startswith('9'): 
+            # returns the area code  - O(1)
             return number[:4]
+    # True if none of above  - O(1)
+    else:
+        # returns an error.
+        return "Invalid phone number"
             
 def print_lex_ordered_numbers(numbers):
+    """
+    This function sorts a list of numbers in lexicographic order and prints each number.
+
+    INPUT: 
+    numbers: a list of numbers. 
+
+    RETURN: None, this function doesn't return anything.
+    """
+    # sorting the list in lexicographic order  - O(n)
     numbers.sort()
+    # iterating the list to print the ordered numbers  - O(1)
     for number in numbers:
         print(number)
 
-def get_numbers_called_by_area_code_to_area_code(area_code, area_code2, calls):
+def get_numbers_called_by_same_area_code(area_code, calls):
+    """
+    This function iterates a list of calls and fill a set with the numbers that match the area codes.
+
+    INPUT: 
+    area_code: the area code that filters the list. 
+    calls: a list of numbers. 
+
+    RETURN: the len of the list of numbers that called other numbers with the same area code.
+    """
+    # creating a set to store unique numbers  - O(1)
     called_numbers = set()
+    # starting the index with 0  - O(1)
     index = 0
+    # iterating over the calls  - O(n)
     while index < len(calls):
-        if calls[index][0].startswith(area_code) and calls[index][1].startswith(area_code2):
+        # compare if the caller number starts with the area code - O(1)
+        if calls[index][0].startswith(area_code) and calls[index][1].startswith(area_code):
+            # adding the caller number to the list  - O(1)
             called_numbers.add(calls[index][0])
+        # incrementing the index by 1  - O(1)
         index += 1
     
+    # returning the length of the called numbers  - O(1)
     return len(called_numbers)
 
 def get_percentage_from_fixed_lines(number_of_calls_from_Bangalore, number_of_calls_from_Bangalore_to_Bangalore):
+    """
+    This function gets the percentage of calls from fixed lines in Bangalore are made 
+    to fixed lines also in Bangalore
+
+    INPUT: # getting the percentage.  - O(1)
+    number_# getting the percentage.  - O(1)of calls from fixed lines in Bangalore. 
+    number_# getting the percentage.  - O(1)lore: number of calls from fixed lines in Bangalore to 
+           # getting the percentage.  - O(1)      fixed lines in Bagalore. 
+
+    RETURN:# getting the percentage.  - O(1)xed lines in Bangalore are made 
+           # getting the percentage.  - O(1)e
+    """
+    # getting the percentage.  - O(1)
     return (number_of_calls_from_Bangalore_to_Bangalore * 100) / number_of_calls_from_Bangalore
 
 def main():
     # Part A solution
+    # defining the area code from Bangalore
+    area_code = "(080)"
+
+    # printing the solution message.  - O(1)
     print("The numbers called by people in Bangalore have codes:")
-    calls_from_Bangalore = get_numbers_called_by_area_code('(080)', calls)
-    print_lex_ordered_numbers(calls_from_Bangalore)
+
+    # getting all of the area codes and mobile prefixes called by people in Bangalore.  - O(n)
+    unique_numbers_from_Bangalore = get_unique_numbers_called_by_area_code(area_code, calls)
+    
+    # ordering and printing the list of calls from Bangalore.  - O(1)
+    print_lex_ordered_numbers(unique_numbers_from_Bangalore)
 
     # Part B solution
+    # getting the numbers of calls from bangalore  - O(n)
+    number_of_calls_from_bangalore = get_number_of_calls_from_area_code(area_code, calls)
+
+    # getting the number of calls from fixed lines in Bangalore to other fixed lines in Bangalore  - O(n)
+    number_of_calls_from_Bangalore_to_Bangalore = get_numbers_called_by_same_area_code(area_code, calls)
+
+    # calculating the percentage from fixed lines in Bangalore are calls to other fixed lines in Bangalore  - O(1)
+    percentage = get_percentage_from_fixed_lines(number_of_calls_from_bangalore, number_of_calls_from_Bangalore_to_Bangalore)
+    
+    # printing the solution message
     print("{:.2f}% percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore."
-        .format(get_percentage_from_fixed_lines(len(calls_from_Bangalore), 
-            get_numbers_called_by_area_code_to_area_code('(080)', '(080)', calls))))
+        .format(percentage))
 
 if __name__ == "__main__":
     main()
-
 
 """
 # UNIT TESTING
@@ -150,22 +250,22 @@ order_test_list1 = ["(080)47459867", "98440 65896", "90087 42537", "(080)3512149
 print_lex_ordered_numbers(order_test_list1)
 print("test for print_lex_ordered_numbers passed")
 
-# get_numbers_called_by_area_code_to_area_code test
+# get_numbers_called_by_same_area_code test
 # test with eigth numbers on the list two callers from Bangalore and one receiver from Bancalore
 bangalore_calls_test_list1 = [["(080)47459867", "98440 65896"], ["90087 42537", "(0471)6537077"], 
                     ["(044)30727085", "92414 22596"], ["(080)23802940", "(080)35121497"],
                     ["78130 00821", "98453 94494"], ["98453 46196", "90352 50054"]]
-#assert(get_numbers_called_by_area_code_to_area_code("(080)", "(080)", bangalore_calls_test_list1) == 1)
+#assert(get_numbers_called_by_same_area_code("(080)", "(080)", bangalore_calls_test_list1) == 1)
 # test with eigth numbers on the list one caller from Bangalore and two receivers from Bancalore
 bangalore_calls_test_list2 = [["98440 65896", "(080)47459867"], ["90087 42537", "(0471)6537077"], 
                     ["(044)30727085", "92414 22596"], ["(080)23802940", "(080)35121497"],
                     ["78130 00821", "98453 94494"], ["98453 46196", "90352 50054"]]
-assert(get_numbers_called_by_area_code_to_area_code("(080)", "(080)", bangalore_calls_test_list1) == 1)
+assert(get_numbers_called_by_same_area_code("(080)", "(080)", bangalore_calls_test_list1) == 1)
 # four numbers with none caller from Bangalore
 bangalore_calls_test_list3 = [["98440 65896", "(080)47459867"], ["90087 42537", "(080)35121497"], 
                     ["(044)30727085", "92414 22596"], ["97447 92655", "(022)39006198"]]
-assert(get_numbers_called_by_area_code_to_area_code("(080)", "(080)", bangalore_calls_test_list3) == 0)
-print("test for get_numbers_called_by_area_code_to_area_code passed")
+assert(get_numbers_called_by_same_area_code("(080)", "(080)", bangalore_calls_test_list3) == 0)
+print("test for get_numbers_called_by_same_area_code passed")
 
 # test filter_area_codes
 # test with a fixed number
--- P0/Task4.py
+++ P0/Task4.py
@@ -12,78 +12,6 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-
-def get_numbers_without_incoming_calls(calls):
-    telemarketers = set() 
-    index = 0
-    total_calls = len(calls)
-
-    while index < total_calls:
-        index2 = 0
-        telemarketer = True
-        while index2 < total_calls:
-            if calls[index][0] == calls[index2][1]:
-                telemarketer = False
-                break    
-            index2 += 1
-        
-        if telemarketer:
-            telemarketers.add(calls[index][0])
-        
-        index  += 1
-    
-    return telemarketers
-
-def get_numbers_without_messages(calls, texts):
-    calls_list = list(calls)
-    calls_index = 0
-    text_index = 0
-
-    while calls_index < len(calls_list):
-        while text_index < len(texts):
-            if calls_list[calls_index] == texts[text_index][0] or calls_list[calls_index] == texts[text_index][1]:
-                calls_list.pop(calls_index)
-            
-            text_index += 1
-        
-        calls_index += 1
-
-    return set(calls_list)
-
-def print_lex_ordered_numbers(telemarketers):
-    numbers = list(telemarketers)
-    numbers.sort()
-    for number in numbers:
-        print(number)
-
-def print_list_of_telemarketers(calls, texts):
-
-    calls = get_numbers_without_incoming_calls(calls)
-    telemarketers = get_numbers_without_messages(calls, texts)
-    print_lex_ordered_numbers(telemarketers)
-
-
-# TEST FOR get_numbers_without_incoming_calls
-# four numbers with one caller from Bangalore
-calls_test_list1 = [["1408371942", "98440 65896"], ["90087 42537", "(044)30727085"], 
-                    ["(044)30727085", "92414 22596"], ["1408371942", "90087 42537"]]
-assert(get_numbers_without_incoming_calls(calls_test_list1) == {"1408371942"})
-#print("test for get_numbers_without_incoming_calls passed")
-
-# TEST FOR get_numbers_without_messages
-# one number at the end
-calls_test_list2 = ["1408371942"]
-text_test_list2 = [["87146 31956", "98440 65896"], ["90087 42537", "(044)30727085"], 
-                    ["93435 21961", "92414 22596"], ["1408371944", "90087 42537"]]
-assert(get_numbers_without_messages(calls_test_list2, text_test_list2) == {"1408371942"})
-# two numbers at the end
-calls_test_list3 = ["1408371942", "1408409918"]
-text_test_list3 = [["87146 31956", "98440 65896"], ["90087 42537", "(044)30727085"], 
-                    ["93435 21961", "92414 22596"], ["1408371944", "90087 42537"]]
-assert(get_numbers_without_messages(calls_test_list3, text_test_list3) == {"1408371942", "1408409918"})
-#print("test for get_numbers_without_messages passed")
-
-
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
@@ -96,5 +24,135 @@ Print a message:
 <list of numbers>
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
-print("These numbers could be telemarketers: ")
-print_list_of_telemarketers(calls, texts)
+
+def get_numbers_without_incoming_calls(calls):
+    """
+    This function returns a set of numbers that does not have incoming calls.
+
+    INPUT:
+    calls: the list of calls
+
+    RETURN: a set of numbers that doesn't have incoming calls.
+    """
+    # creating the set  - O(1)
+    telemarketers = set() 
+    # creating the index  - O(1)
+    index = 0
+    # getting the length of the calls list  - O(1)
+    total_calls = len(calls)
+
+    # iterating over the list of calls  - O(n)
+    while index < total_calls:
+        # creating a second index  - O(1)
+        index2 = 0
+        # creating a booleand variable to know if the number haven't receive a call  - O(1)
+        telemarketer = True
+
+        # iterating the list again to look if the number haven't receive a call  - O(n)
+        while index2 < total_calls:
+            # checking if the caller number is on the receivers numbers  - O(1)
+            if calls[index][0] == calls[index2][1]:
+                # if the number is on the receivers numbers then is not a telemarketer  - O(1)
+                telemarketer = False
+                # breaking the cicle, it's not important to search over the complete list  - O(1)
+                break
+            # increasing the second index by 1  - O(1)    
+            index2 += 1
+        
+        # if was a telemarketer  - O(1)
+        if telemarketer:
+            # if was a telemarketer, then add the number to the list  - O(1)
+            telemarketers.add(calls[index][0])
+        
+        # increase the index by 1  - O(1)
+        index  += 1
+    
+    # converting the set to a list, and returning the list of possible telemarketers  - O(n)
+    return list(telemarketers)
+
+def get_numbers_without_messages(calls, texts):
+    """
+    This function given a set of calls and a list of texts, checks if the numbers on the set of calls
+    never send texts or receive texts.
+
+    INPUT:
+    calls: a set of calls
+    texts: a list if numbers that have sent and receive texts.
+
+    RETURN: a set of numbers that never send texts or receive texts.
+    """
+    # creating an index  - O(1)
+    calls_index = 0
+    # creating a second index  - O(1)
+    text_index = 0
+
+    # iterating the list of calls  - O(n)
+    while calls_index < len(calls):
+        # iterating the list of texts  - O(n)
+        while text_index < len(texts):
+            # checking if the number on the list of calls is on the sender text or receiver text  - O(1)
+            if calls[calls_index] == texts[text_index][0] or calls[calls_index] == texts[text_index][1]:
+                # deleting the number if it's on the list of texts.  - O(1)
+                calls.pop(calls_index)
+            
+            # increasing the second index by 1 - O(1)
+            text_index += 1
+        
+        # increasing the index by 1 - O(1)
+        calls_index += 1
+
+    # returning the list of numbers - O(1)
+    return calls
+
+def print_lex_ordered_numbers(telemarketers):
+    """
+    This function sorts a set of numbers in lexicographic order and prints each number.
+
+    INPUT: 
+    numbers: a list of numbers. 
+
+    RETURN: None, this function doesn't return anything.
+    """
+    # sorting the list - O(n)
+    telemarketers.sort()
+    # iterating the list to print the numbers - O(n)
+    for number in telemarketers:
+        # printing the numbers
+        print(number)
+
+def main():
+    # getting the set of calls without numbers that receive a call - O(nx2)
+    set_calls = get_numbers_without_incoming_calls(calls)
+    # getting numbers that have never send or receive a text  - O(nx2)
+    telemarketers = get_numbers_without_messages(set_calls, texts)
+    # printing the message of the solution.  - O(1)
+    print("These numbers could be telemarketers: ")
+    # printing those numbers.  - O(n)
+    print_lex_ordered_numbers(telemarketers)
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+# UNIT TESTING
+# get_numbers_without_incoming_calls test
+# four numbers with one caller frget_numbers_called_by_same_area_codeom Bangalore
+calls_test_list1 = [["1408371942", "98440 65896"], ["90087 42537", "(044)30727085"], 
+                    ["(044)30727085", "92414 22596"], ["1408371942", "90087 42537"]]
+assert(get_numbers_without_incoming_calls(calls_test_list1) == ["1408371942"])
+# print("test for get_numbers_without_incoming_calls passed")
+
+# get_numbers_without_messages test
+# one number at the end
+calls_test_list2 = ["1408371942"]
+text_test_list2 = [["87146 31956", "98440 65896"], ["90087 42537", "(044)30727085"], 
+                    ["93435 21961", "92414 22596"], ["1408371944", "90087 42537"]]
+assert(get_numbers_without_messages(calls_test_list2, text_test_list2) == ["1408371942"])
+# two numbers at the end
+calls_test_list3 = ["1408371942", "1408409918"]
+text_test_list3 = [["87146 31956", "98440 65896"], ["90087 42537", "(044)30727085"], 
+                    ["93435 21961", "92414 22596"], ["1408371944", "90087 42537"]]
+assert(get_numbers_without_messages(calls_test_list3, text_test_list3) == ["1408371942", "1408409918"])
+#print("test for get_numbers_without_messages passed")
+"""
