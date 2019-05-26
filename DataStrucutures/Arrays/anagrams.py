6c48eeebc300ccd781570e4a1a0b2a4f969c7bf0
Marco Tello <muchahuev@hotmail.com>
2019-05-26 15:43:44 -0500

    adding data structures practices

---
+++ DataStrucutures/Arrays/anagrams.py
@@ -0,0 +1,30 @@
+def anagram_checker(str1, str2):
+
+    """
+    Check if the input strings are anagrams of each other
+
+    Args:
+       str1(string),str2(string): Strings to be checked
+    Returns:
+       bool: Indicates whether strings are anagrams
+    """
+    
+    # TODO: Write your solution here
+    result = False
+
+    for word in str1:
+        if str1.count(word) == str2.count(word):
+            result = True
+        else:
+            result = False
+            break
+
+    return result
+
+print(anagram_checker('water','waiter'))
+
+print ("Pass" if not (anagram_checker('water','waiter')) else "Fail")
+print ("Pass" if anagram_checker('Dormitory','Dirty room') else "Fail")
+print ("Pass" if anagram_checker('Slot machines', 'Cash lost in me') else "Fail")
+print ("Pass" if not (anagram_checker('A gentleman','Elegant men')) else "Fail")
+print ("Pass" if anagram_checker('Time and tide wait for no man','Notified madman into water') else "Fail")
---
+++ DataStrucutures/Arrays/reverse_string.py
@@ -0,0 +1,26 @@
+def string_reverser(our_string):
+
+    """
+    Reverse the input string
+
+    Args:
+       our_string(string): String to be reversed
+    Returns:
+       string: The reversed string
+    """
+    
+    # TODO: Write your solution here
+    index = len(our_string)
+    reversed = ""
+
+    while index > 0:
+        reversed = reversed + our_string[index-1]
+        index -= 1
+
+    return reversed
+
+example = "holas"
+
+print ("Pass" if ('retaw' == string_reverser('water')) else "Fail")
+print ("Pass" if ('!noitalupinam gnirts gnicitcarP' == string_reverser('Practicing string manipulation!')) else "Fail")
+print ("Pass" if ('3432 :si edoc esuoh ehT' == string_reverser('The house code is: 2343')) else "Fail")
