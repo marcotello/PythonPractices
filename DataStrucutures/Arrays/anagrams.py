df0d6549f40584490d7ff6f38dce69b4d34afa11
Marco Tello <muchahuev@hotmail.com>
2019-05-27 23:12:41 -0500

    ading the anagram solution

--- DataStrucutures/Arrays/anagrams.py
+++ DataStrucutures/Arrays/anagrams.py
@@ -1,3 +1,34 @@
+def count_characters(word):
+    words = dict()
+
+    word = word.casefold()
+
+    for char in word:
+        if char != " ":
+            if char in words:
+                words[char] = words[char] + 1
+            else:
+                words[char] = 1
+    
+    return words
+
+def compare_dictionaries(dict1, dict2):
+
+    result = True
+
+    for element in dict1:
+        if element in dict2:
+            if dict1[element] == dict2[element]:
+                result = True
+            else:
+                result = False
+                break
+        else:
+            result = False
+            break
+    
+    return result
+
 def anagram_checker(str1, str2):
 
     """
@@ -10,21 +41,32 @@ def anagram_checker(str1, str2):
     """
     
     # TODO: Write your solution here
-    result = False
 
-    for word in str1:
-        if str1.count(word) == str2.count(word):
-            result = True
-        else:
-            result = False
-            break
+    str1_dict = count_characters(str1)
+    str2_dict = count_characters(str2)
 
-    return result
+    return compare_dictionaries(str1_dict, str2_dict)
 
-print(anagram_checker('water','waiter'))
 
-print ("Pass" if not (anagram_checker('water','waiter')) else "Fail")
-print ("Pass" if anagram_checker('Dormitory','Dirty room') else "Fail")
-print ("Pass" if anagram_checker('Slot machines', 'Cash lost in me') else "Fail")
-print ("Pass" if not (anagram_checker('A gentleman','Elegant men')) else "Fail")
-print ("Pass" if anagram_checker('Time and tide wait for no man','Notified madman into water') else "Fail")
+
+'''
+def main():
+    print(anagram_checker('water','waiter'))    
+
+    print ("Pass" if not (anagram_checker('water','waiter')) else "Fail")
+    print ("Pass" if anagram_checker('Dormitory','Dirty room') else "Fail")
+    print ("Pass" if anagram_checker('Slot machines', 'Cash lost in me') else "Fail")
+    print ("Pass" if not (anagram_checker('A gentleman','Elegant men')) else "Fail")
+    print ("Pass" if anagram_checker('Time and tide wait for no man','Notified madman into water') else "Fail")    
+
+
+if __name__ == "__main__":
+    main()
+'''
+
+
+# UNIT TESTING 
+# count_characters test
+assert(count_characters("Hello World Hello World Hello World") == {'h': 3, 'e': 3, 'l': 9, 'o': 6, 'w': 3, 'r': 3, 'd': 3})
+assert(count_characters("This is a test") == {'t': 3, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 1})
+print("test for count_characters passed")
