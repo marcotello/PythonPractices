7e67e153bf925ca4c19ecfc09a07d37b868284ce
Marco Tello <muchahuev@hotmail.com>
2019-06-25 23:32:49 -0500

    starting recursion tree

---
+++ Trees/recursion_trees.py
@@ -0,0 +1,86 @@
+# Let's define a stack to help keep track of the tree nodes
+class Stack():
+    def __init__(self):
+        self.list = list()
+        
+    def push(self,value):
+        self.list.append(value)
+        
+    def pop(self):
+        return self.list.pop()
+        
+    def top(self):
+        if len(self.list) > 0:
+            return self.list[-1]
+        else:
+            return None
+        
+    def is_empty(self):
+        return len(self.list) == 0
+    
+    def __repr__(self):
+        if len(self.list) > 0:
+            s = "<top of stack>\n_________________\n"
+            s += "\n_________________\n".join([str(item) for item in self.list[::-1]])
+            s += "\n_________________\n<bottom of stack>"
+            return s
+        
+        else:
+            return "<stack is empty>"
+
+class Node(object):
+        
+    def __init__(self,value = None):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.visited_l = False
+        self.visited_r = False
+        
+    def set_value(self,value):
+        self.value = value
+        
+    def get_value(self):
+        return self.value
+        
+    def set_left_child(self,left):
+        self.left = left
+        
+    def set_right_child(self, right):
+        self.right = right
+        
+    def get_left_child(self):
+        return self.left
+    
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left != None
+    
+    def has_right_child(self):
+        return self.right != None
+    
+    def visited_left(self, value):
+        self.visited_l = value
+    
+    def visited_right(self, value):
+        self.visited_r = value
+    
+    def get_visited_left(self):
+        return self.visited_l
+    
+    def get_visited_right(self):
+        return self.visited_r
+    
+    # define __repr_ to decide what a print statement displays for a Node object
+    def __repr__(self):
+        return f"Node({self.get_value()})"
+    
+    def __str__(self):
+        return f"Node({self.get_value()})"
+
+
+
+def pre_order(tree):
+    pass
