b44c4c1d46c9ecbe5030445d23e129ccc6797146
Marco Tello <muchahuev@hotmail.com>
2019-07-03 16:52:04 -0500

    addint the BFS with a Queue

--- Trees/bfs_algorithm.py
+++ Trees/bfs_algorithm.py
@@ -73,96 +73,70 @@ class Tree():
         
     def get_root(self):
         return self.root
+    
+    """
+    define the print function
+    """
+    def __repr__(self):
+        level = 0
+        q = Queue()
+        visit_order = list()
+        node = self.get_root()
+        q.enq( (node,level) )
+        while(len(q) > 0):
+            node, level = q.deq()
+            if node == None:
+                visit_order.append( ("<empty>", level))
+                continue
+            visit_order.append( (node, level) )
+            if node.has_left_child():
+                q.enq( (node.get_left_child(), level +1 ))
+            else:
+                q.enq( (None, level +1) )
+                
+            if node.has_right_child():
+                q.enq( (node.get_right_child(), level +1 ))
+            else:
+                q.enq( (None, level +1) )
+                
+        s = "Tree\n"
+        previous_level = -1
+        for i in range(len(visit_order)):
+            node, level = visit_order[i]
+            if level == previous_level:
+                s += " | " + str(node) 
+            else:
+                s += "\n" + str(node)
+                previous_level = level
+
+        return s
+        
+        
+
+# BFS algorithm
+def bfs(tree):
+    visit_order = list()
+    q = Queue()
+
+    node = tree.get_root()
+    q.enq(node)
+
+    while len(q) > 0:
+        
+        node = q.deq()
+        visit_order.append(node)
+        
+        if node.has_left_child():
+            q.enq(node.get_left_child())
+        if node.has_right_child():
+            q.enq(node.get_right_child())
+    
+    return visit_order
 
 tree = Tree("apple")
 tree.get_root().set_left_child(Node("banana"))
 tree.get_root().set_right_child(Node("cherry"))
 tree.get_root().get_left_child().set_left_child(Node("dates"))
 
-'''
-q = deque()
-q.appendleft("apple")
-q.appendleft("banana")
-print(q)
-q.pop()
-print(q)
-print(len(q))
-
-#########################333
-q = Queue()
-q.enq("apple")
-q.enq("banana")
-q.enq("cherry")
-print(q)
-
-print(q.deq())
-
-print(q)
-'''
-############################333
-
-visit_order = list()
-q = Queue()
-
-# start at the root node and add it to the queue
-node = tree.get_root()
-q.enq(node)
-print(q)
-print("\n------------------------------------------------")
-
-# dequeue the next node in the queue. 
-# "visit" that node
-# also add its children to the queue
-
-node = q.deq()
-visit_order.append(node)
-if node.has_left_child():
-    q.enq(node.get_left_child())
-if node.has_right_child():
-    q.enq(node.get_right_child())
-    
+visit_order = bfs(tree)
 print(f"visit order: {visit_order}")
-print(q)
-print("\n------------------------------------------------")
-
-# dequeue the next node (banana)
-# visit it, and add its children (dates) to the queue 
-
-node = q.deq()
-visit_order.append(node)
-if node.has_left_child():
-    q.enq(node.get_left_child())
-if node.has_right_child():
-    q.enq(node.get_right_child())
-    
-print(f"visit order: {visit_order}")
-print(q)
-print("\n------------------------------------------------")
-
-# dequeue the next node (cherry)
-# visit it, and add its children (there are None) to the queue 
-
-node = q.deq()
-visit_order.append(node)
-if node.has_left_child():
-    q.enq(node.get_left_child())
-if node.has_right_child():
-    q.enq(node.get_right_child())
-    
-print(f"visit order: {visit_order}")
-print(q)
-print("\n------------------------------------------------")
-
-# dequeue the next node (dates)
-# visit it, and add its children (there are None) to the queue 
-
-node = q.deq()
-visit_order.append(node)
-if node.has_left_child():
-    q.enq(node.get_left_child())
-if node.has_right_child():
-    q.enq(node.get_right_child())
-    
-print(f"visit order: {visit_order}")
-print(q)
-print("\n------------------------------------------------")
