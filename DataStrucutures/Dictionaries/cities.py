ca4a81e69939db526c6125a1143d6e8cf0230bd4
Marco Tello <muchahuev@hotmail.com>
2019-07-08 22:56:24 -0500

    finishing the cities exersise

--- DataStrucutures/Dictionaries/cities.py
+++ DataStrucutures/Dictionaries/cities.py
@@ -12,4 +12,11 @@ for city in american_cities:
     print(city)
 
 # TODO: Print all cities in Asia, in alphabetic order, next to the name of the country
-asean_cities = ['Asia']
+asian_countries = locations['Asia']
+asian_cities = list()
+print(2)
+for country in asian_countries:
+     asian_cities.append("{} - {}".format(asian_countries[country][0], country))
+asian_cities.sort()
+for city in asian_cities:
+     print(city)
